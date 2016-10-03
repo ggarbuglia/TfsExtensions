e69bda9d4f667b115ad67817e417be9e55be3a21
German Garbuglia <german.garbuglia@gmail.com>
2016-10-03 10:37:20 -0300

    Version 0.2.0
    Powershell code cleanup.
    7z.exe existence validation added.
    Target folder retension days (cleanup) added.

--- ArchiveRemoteFolder/task.ps1
+++ ArchiveRemoteFolder/task.ps1
@@ -5,52 +5,71 @@ param (
     [string] $sourcepath,
     [string] $targetpath,
     [string] $filename,
-    [string] $datestampformat
+    [string] $datestampformat = 'yyyyMMddhhmm',
+    [string] $retensiondays = '0'
 )
 
 Write-Host "Entering script task.ps1";
 
-Write-Verbose "Remote Server:      $server";
-Write-Verbose "Admin User:         $adminusr";
-Write-Verbose "Remote Source Path: $sourcepath";
-Write-Verbose "Remote Target Path: $targetpath";
-Write-Verbose "7z Filename:        $filename";
-Write-Verbose "Date Stamp Format:  $datestampformat";
+Write-Verbose "[server]          --> [$server]"          -Verbose;
+Write-Verbose "[adminusr]        --> [$adminusr]"        -Verbose;
+Write-Verbose "[sourcepath]      --> [$sourcepath]"      -Verbose;
+Write-Verbose "[targetpath]      --> [$targetpath]"      -Verbose;
+Write-Verbose "[filename]        --> [$filename]"        -Verbose;
+Write-Verbose "[datestampformat] --> [$datestampformat]" -Verbose;
 
-Write-Host "Validating Source Path variable.";
-
-if (-not $sourcepath.EndsWith("\")) {
-    $sourcepath = "$sourcepath\";
+### Validates all paths ###
+function ValidateSourcePath ([string]$type, [string]$path) {
+    Write-Host "Validating $type Path variable.";
+    if (-not $path.EndsWith("\")) { $path = "$path\"; }
+    Write-Host "$type Path is $path.";
+    return $path;
 }
 
-Write-Host "Source Path is $sourcepath.";
-Write-Host "Validating Target Path variable.";
-
-if (-not $targetpath.EndsWith("\")) {
-    $targetpath = "$targetpath\";
+### Validates 7z file name ###
+function ValidateFilename ([string]$name, [string]$format) {
+    Write-Host "Validating Filename variable.";
+    if (-not $name.EndsWith(".7z")) {
+        $name = "$name" + "_" + (Get-Date -format $format) + ".7z";
+    } else {
+        $name = $name.Replace(".7z", "") + "_" + (Get-Date -format $format) + ".7z";
+    }
+    Write-Host "Filename is $name.";
+    return $name;
 }
 
-Write-Host "Target Path is $targetpath.";
-Write-Host "Validating Filename variable.";
+Write-Host "Creating Script Block.";
+$scriptblock = { 
+    $sourcePath    = $args[0];
+    $targetPath    = $args[1];
+    $fileName      = $args[2];
+    $retensionDays = $args[3];
 
-if (-not $filename.EndsWith(".7z")) {
-    $filename = "$filename" + "_" + (Get-Date -format $datestampformat) + ".7z";
-} else {
-    $filename = $filename.Replace(".7z", "") + "_" + (Get-Date -format $datestampformat) + ".7z";
-}
+    if (-not (Test-Path $sourcePath)) { throw "Source path '$sourcePath' does not exist."; }
+    if (-not (Test-Path $targetPath)) { throw "Target path '$targetPath' does not exist."; }
 
-Write-Host "Filename is $filename.";
+    if ($retensionDays -ne "0") {
+        $days = [Convert]::ToInt32($retensionDays, 10);
+        $limit = (Get-Date).AddDays(-$days);
+        Get-ChildItem $targetPath | ? { $_.Extension -eq ".7z" -and $_.CreationTime -lt $limit } | Remove-Item;
+    }
 
-$Source = "$sourcepath*.*"
-$7zFile = "$targetpath$filename";
+    if (-not (Test-Path "$env:ProgramFiles\7-Zip\7z.exe")) { throw "$env:ProgramFiles\7-Zip\7z.exe needed"; } 
+    Set-Alias 7z "$env:ProgramFiles\7-Zip\7z.exe"; 
+    7z a -t7z "$targetPath$fileName" "$sourcePath*.*" 
+};
+
+$sourcepath = ValidateSourcePath -type "Source" -path $sourcepath;
+$targetpath = ValidateSourcePath -type "Target" -path $targetpath;
+$filename   = ValidateFilename -name $filename -format $datestampformat;
 
 Write-Host "Creating Secured Credentials.";
-$Credential = New-Object System.Management.Automation.PSCredential($adminusr, (ConvertTo-SecureString -String $adminpwd -AsPlainText -Force));
+$credential = New-Object System.Management.Automation.PSCredential($adminusr, (ConvertTo-SecureString -String $adminpwd -AsPlainText -Force));
 
 Write-Host "Opening Powershel remote session on $server.";
-$Session = New-PSSession -ComputerName $server -Credential $Credential;
+$session = New-PSSession -ComputerName $server -Credential $credential;
 
 Write-Host "Invoking 7z executable on remote server.";
-Invoke-Command -Session $Session { Set-Alias 7z "$env:ProgramFiles\7-Zip\7z.exe"; 7z a -t7z $args[0] $args[1] } -ArgumentList $7zFile, $Source;
+Invoke-Command -Session $session -ScriptBlock $scriptblock -ArgumentList $sourcepath, $targetpath, $filename, $retensiondays;
 
 Write-Host "Leaving script task.ps1";
--- overview.md
+++ overview.md
@@ -1,5 +1,4 @@
-# Task
-Archive Remote Folder Task
+# Archive Remote Folder Task
 
 ## Details
 This task creates a powershell session to your remote server using the provided server admin credentials.
@@ -9,3 +8,22 @@ Usefull for servers without shared folders.
 ## Requirements
 Your remote server must have WinRM enabled and configured.
 Your remote server must have 7z installed.
+
+## Configuration
+Remote Machine (required) = The FQDN of the remote machine you want to reach.
+Admin Login (required) = The administrator login account [domain\username] for the remote machine.
+Admin Password (required) = The administrator password for the remote machine. I recommend a variable marked as secret.
+Source Path (required) = A local path on the remote server.
+Target Path (required) = A local path on the remote server or a network share where the user has write permissions.
+7z Filename (required) = A file name, with or without extension.
+Date Stamp Format = A file name datetime format.
+Retension Days = The archive folder retension in days.
+
+## Versions
+0.2.0
+Powershell code cleanup.
+7z.exe existence validation added.
+Target folder retension days (cleanup) added.
+
+0.1.9 
+Initial versión on Visual Studio MarketPlace.
